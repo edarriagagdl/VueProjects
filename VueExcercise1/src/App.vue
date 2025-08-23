426b10dd941bee1cf7fc2bd017e340fa5b755bf7
edarriagagdl <edarriaga@gmail.com>
2025-08-22 19:23:49 -0600

    Agregar primer ejercicio de view un toDoList

---
+++ VueExcercise1/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+import TodoItem from './components/TodoItem.vue';
+
+const newTask = ref('');
+const tasks = ref([]);
+
+function addTask() {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value.trim());
+    newTask.value = '';
+  }
+}
+
+function removeTask(index) {
+  tasks.value.splice(index, 1);
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="newTask" @keyup.enter="addTask" />
+    <button @click="addTask">Agregar</button>
+    <TodoItem
+      v-for="(task, index) in tasks"
+      :key="index"
+      :text="task"
+      @delete="removeTask(index)"
+    />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ VueExcercise1/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  text: String,
+});
+
+const emit = defineEmits(['delete']);
+function handleDelete() {
+  emit('delete');
+}
+</script>
+
+<template>
+  <div>
+    <span>{{ text }}</span>
+    <button @click="handleDelete">Delete</button>
+  </div>
+</template>
+
+<style scoped></style>
